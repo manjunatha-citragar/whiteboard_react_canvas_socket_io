62112f2f8f0a863b0f130a946f2f50203ce766c9
manjunatha c <manjunathagee@gmail.com>
2023-05-28 07:09:25 +0000

    Integrate text writing and socket io changes.

--- src/components/Whiteboard/utils/createElement.js
+++ src/components/Whiteboard/utils/createElement.js
@@ -11,7 +11,7 @@ const generateLine = ({ x1, x2, y1, y2 }) => {
   return generator.line(x1, y1, x2, y2);
 };
 
-export const createElement = ({ x1, x2, y1, y2, toolType, id }) => {
+export const createElement = ({ x1, x2, y1, y2, toolType, id, text = "" }) => {
   let roughElement;
 
   switch (toolType) {
@@ -43,6 +43,15 @@ export const createElement = ({ x1, x2, y1, y2, toolType, id }) => {
         id,
         type: toolType,
         points: [{ x: x1, y: y1 }],
+      };
+
+    case toolTypes.TEXT:
+      return {
+        id,
+        type: toolType,
+        x1,
+        y1,
+        text: text,
       };
 
     default:
