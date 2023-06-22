84ee5f806473df6f9725e5c766af6fb29fcb0450
manjunatha C <manjunatha.citragar@expinfi.com>
2023-06-22 16:10:13 +0530

    Playrecorded bug fix.

--- src/components/Whiteboard/utils/updateLocalStore.js
+++ src/components/Whiteboard/utils/updateLocalStore.js
@@ -2,10 +2,12 @@ import { whiteboardEvents } from "../../../constants";
 import { updateCursorPosition } from "../../../CursorOverlay/cursorSlice";
 import store from "../../../store/store";
 import { setElements, setToolType, updateElement } from "../whiteboardSlice";
+import { deserializeData } from "./deserializeData";
 
 export const updateLocalStore = (payload) => {
   if (payload.type === whiteboardEvents.UPDATE_ELEMENT) {
-    store.dispatch(updateElement(payload.data));
+    const data = deserializeData(payload);
+    store.dispatch(updateElement(data.data ? data.data : data));
   } else if (payload.type === whiteboardEvents.UPDATE_CURSOR_POSITION) {
     store.dispatch(updateCursorPosition(payload.data));
   } else if (payload.type === whiteboardEvents.CLEAR_WHITEBOARD) {
